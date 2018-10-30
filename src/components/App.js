6b234ca579f06ded973e77204323aa54fe86f17f
Kate Dubitski <kate.dubitski@gmail.com>
2018-10-30 15:28:28 -0400

    passing props to card component and rendering them

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,16 @@ import logo from '../logo.svg';
 import '../App.css';
 import Card from './Card';
 
+const Data = {
+  user: {
+    name: 'Kate',
+    image: 'https://miro.medium.com/fit/c/240/240/0*yIBEf5iZoTdv3CMH.',
+    quote: [
+      `I can’t give you a sure-fire formula for success, but I can give you a formula for failure: try to please everybody all the time.`,
+      '-Herbert Bayard Swope',
+    ],
+  },
+};
 class App extends Component {
   render() {
     return (
@@ -24,7 +34,7 @@ class App extends Component {
         </header>
         <div>
           <p>content</p>
-          <Card />
+          <Card user={Data.user} />
         </div>
       </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-function Card() {
+function Card(props) {
+  const { name, image, quote } = props.user;
+  console.log(props);
   return (
     <div>
       <h1>user card</h1>
+      <h3>{name}</h3>
+      <div>
+        <p>{quote[0]}</p>
+        <h6>{quote[1]}</h6>
+      </div>
+      <img alt="Smiling girl catching a snowflake" src={image} />
     </div>
   );
 }
