1f3d5e65de4aeb13585450d91c457db93cc4c5aa
Kate Dubitski <kate.dubitski@gmail.com>
2018-10-30 15:43:31 -0400

    added class names to card component to enable css styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,14 +3,18 @@ import React from 'react';
 function Card(props) {
   const { name, image, quote } = props.user;
   return (
-    <div>
+    <div className="userCard">
       <h1>user card</h1>
       <h3>{name}</h3>
-      <div>
+      <div className="userQuote">
         <p>{quote[0]}</p>
         <h6>{quote[1]}</h6>
       </div>
-      <img alt="Smiling girl catching a snowflake" src={image} />
+      <img
+        alt="Smiling girl catching a snowflake"
+        src={image}
+        className="userImage"
+      />
     </div>
   );
 }
