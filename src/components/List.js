70e0428abeeadccbf3b7e7a0a4e8fea7359d4c41
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-01 18:27:50 -0400

    added class name to the List component for css styling

--- src/components/List.js
+++ src/components/List.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 function List(props) {
   return (
-    <div>
+    <div className="userList">
       {props.userList.map(item => (
         <Card key={item.id} user={item} />
       ))}
