d75790611a478381e4da6921f2c2a3746e07bea6
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-02 16:09:04 -0400

    added new div to Card component and new class name, added styling to new class innerUserCard

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,12 @@ function Card(props) {
   return (
     <div className="userCard">
       <h2>{name}</h2>
-      <div className="userQuote">
-        <p>{quote}</p>
+      <div className="innerUserCard">
+        <div className="userQuote">
+          <p>{quote}</p>
+        </div>
+        <img alt={`${name} avatar`} src={image} className="userImage" />
       </div>
-      <img alt={`${name} avatar`} src={image} className="userImage" />
     </div>
   );
 }
