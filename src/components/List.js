7ce21b5e5137df5425d7daaab408138e852a2e0f
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-05 17:19:07 -0500

    added no users rendering if users array is empty

--- src/components/List.js
+++ src/components/List.js
@@ -3,6 +3,9 @@ import Card from './Card';
 
 function List(props) {
   console.log(props);
+  if (!props.userList.length) {
+    return <h1>No Users Available</h1>;
+  }
   return (
     <div className="userList">
       {props.userList.map(item => (
