f4d00263e4914921e40ba56b6f89a3a3f12780b7
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-15 16:42:57 -0500

    taking out the theme picture out of the theme component directly to the user card component

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import UserTheme from './UserTheme';
+// import UserTheme from './UserTheme';
 
 function UserCard(props) {
   const { name, image, theme } = props.user;
 
   return (
-    <div className="userCard">
-      <UserTheme theme={theme} />
+    <div className="dashboardProfileCard">
+      <img alt={`user theme`} src={theme} />
       <img className="userImage" alt={`${name} avatar`} src={image} />
       <h3 className="userName">{name}</h3>
     </div>
