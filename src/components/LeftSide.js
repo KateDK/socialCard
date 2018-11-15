4f1b4d7fecde7d45d7ac1eacaac3c33ce8dadd3c
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-15 15:47:56 -0500

    managed to render props in LeftSide component

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -2,14 +2,22 @@ import React from 'react';
 import Card from './Card';
 
 function LeftSide(props) {
-  console.log('props ', props.user);
-  if (!props) {
+  if (!props.user) {
     return <h1>No Data</h1>;
   }
+  console.log('props ', props.user.name);
 
   return (
     <div>
       <h1>some content</h1>
+
+      <img
+        className="userImage"
+        alt={`${props.user.name} avatar`}
+        src={props.user.image}
+      />
+      <h3 className="userName">{props.user.name}</h3>
+      <img alt={`${props.user.name} theme`} src={props.user.theme} />
     </div>
   );
 }
