0c54035d3e2ffde82d9a09c832b30f96e84dacb4
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-15 16:34:41 -0500

    LeftSide component code cleanup

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -6,18 +6,10 @@ function LeftSide(props) {
   if (!props.user) {
     return <h1>No Data</h1>;
   }
-  // console.log('props ', props.user.name);
-
   return (
     <div>
       <h1>some content</h1>
       <UserTheme theme={props.user.theme} />
-      {/* <img
-        className="userImage"
-        alt={`${props.user.name} avatar`}
-        src={props.user.image}
-      />
-      <h3 className="userName">{props.user.name}</h3> */}
       <UserCard user={props.user} />
     </div>
   );
