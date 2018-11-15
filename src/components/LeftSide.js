c063f362968862a3863dabdd624adc6b1ba7797a
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-15 15:54:19 -0500

    UserTheme renders image that it recieves through props

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -6,19 +6,18 @@ function LeftSide(props) {
   if (!props.user) {
     return <h1>No Data</h1>;
   }
-  console.log('props ', props.user.name);
+  // console.log('props ', props.user.name);
 
   return (
     <div>
       <h1>some content</h1>
-      <UserTheme />
+      <UserTheme theme={props.user.theme} />
       <img
         className="userImage"
         alt={`${props.user.name} avatar`}
         src={props.user.image}
       />
       <h3 className="userName">{props.user.name}</h3>
-      <img alt={`${props.user.name} theme`} src={props.user.theme} />
     </div>
   );
 }
--- src/components/UserTheme.js
+++ src/components/UserTheme.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function UserTheme(props) {
+  console.log(props);
   return (
     <div>
       <h1>User Theme Image</h1>
+      <img alt={`user theme`} src={props.theme} />
     </div>
   );
 }
