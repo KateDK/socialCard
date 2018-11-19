0cd2bddfbc423328f5bb64f702da440ebc62082e
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-19 15:57:02 -0500

    prepping dashboard card for displaying numbers of user stats

--- src/components/DashboardProfileCard.js
+++ src/components/DashboardProfileCard.js
@@ -9,6 +9,14 @@ function DashboardProfileCard(props) {
       <img className="theme" alt={`user theme`} src={theme} />
       <img className="userImage" alt={`${name} avatar`} src={image} />
       <h3 className="userName">{name}</h3>
+      <div className="userNumbers">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+      </div>
     </div>
   );
 }
