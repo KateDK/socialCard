089719baf2fc5ec99e63e9c4396e813d7700d286
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-19 16:07:52 -0500

    now rendering user handle on dashboardProfileCard

--- src/components/DashboardProfileCard.js
+++ src/components/DashboardProfileCard.js
@@ -2,13 +2,16 @@ import React from 'react';
 // import UserTheme from './UserTheme';
 
 function DashboardProfileCard(props) {
-  const { name, image, theme } = props.user;
+  const { name, image, theme, handle } = props.user;
 
   return (
     <div className="dashboardProfileCard">
       <img className="theme" alt={`user theme`} src={theme} />
       <img className="userImage" alt={`${name} avatar`} src={image} />
-      <h3 className="userName">{name}</h3>
+      <span className="userName">
+        <h3>{name}</h3>
+        <p className="userHandle">{`@${handle}`}</p>
+      </span>
       <div className="userNumbers">
         <p>1</p>
         <p>2</p>
