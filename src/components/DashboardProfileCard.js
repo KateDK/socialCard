e54937f315567e172c5043fe9644724ed9a36c92
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-19 16:44:37 -0500

    design sorta looks like it did before..

--- src/components/DashboardProfileCard.js
+++ src/components/DashboardProfileCard.js
@@ -2,23 +2,25 @@ import React from 'react';
 // import UserTheme from './UserTheme';
 
 function DashboardProfileCard(props) {
-  const { name, image, theme, handle } = props.user;
+  const { name, image, theme, handle, tweets, folowers, folowing } = props.user;
 
   return (
     <div className="dashboardProfileCard">
-      <img className="theme" alt={`user theme`} src={theme} />
-      <img className="userImage" alt={`${name} avatar`} src={image} />
+      <div className="dashboardImages">
+        <img className="theme" alt={`user theme`} src={theme} />
+        <img className="userImage" alt={`${name} avatar`} src={image} />
+      </div>
       <span className="userName">
         <h3>{name}</h3>
         <p className="userHandle">{`@${handle}`}</p>
       </span>
       <div className="userNumbers">
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
+        <p>Tweets</p>
+        <p>Followers</p>
+        <p>Following</p>
+        <p className="userNumbersItem">{tweets}</p>
+        <p className="userNumbersItem">{folowers}</p>
+        <p className="userNumbersItem">{folowing}</p>
       </div>
     </div>
   );
